d3b014ed0bb6cdf3de87d3f509fe2dd55d81729f
unknown <kinekarpallavi21@gmail.com>
2022-03-28 12:48:20 +0530

    Adding data into node

--- DataStructure/LinkedList.cs
+++ DataStructure/LinkedList.cs
@@ -15,5 +15,39 @@ namespace DataStructure
             Head = null;
             Tail = null;
         }
+        public void AddNode(Node node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+            }
+            else
+            {
+                Console.WriteLine("Linked List as follws");
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                {
+                    Console.Write("->");
+                }
+                temp = temp.next;
+            }
+        }
     }
 }
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -7,9 +7,16 @@ namespace DataStructure
         public static void Main(string[] args)
         {
             LinkedList linkedlist = new LinkedList();
-            Node first_node = new Node(56);
+            Console.WriteLine("First Created is 70");
+            Node first_node = new Node(70);
+            Console.WriteLine("Next 30 added");
             Node second_node = new Node(30);
-            Node third_node = new Node(70);
+            Console.WriteLine("Finally 56 is added");
+            Node third_node = new Node(56);
+            linkedlist.AddNode(first_node);
+            linkedlist.AddNode(second_node);
+            linkedlist.AddNode(third_node);
+            linkedlist.Display();
         }
     }
 }
